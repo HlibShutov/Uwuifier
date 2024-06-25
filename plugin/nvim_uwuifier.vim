04c55bef4444feeef2e5a6a68a6fb55afb74a061
Hlib <u140_hlishu_waw@technischools.com>
2024-06-25 16:03:09 +0200

    multi line comments

--- plugin/nvim_uwuifier.vim
+++ plugin/nvim_uwuifier.vim
@@ -8,6 +8,7 @@ python3 uwuifier = nvim_uwuifier.uwuifier.Uwuifier(vim)
 function! UwuifyRange(start, end)
   python3 start = int(vim.eval('a:start')) - 1
   python3 end = int(vim.eval('a:end'))
+  python3 print(start, end)
   python3 uwuifier.uwuify_range(start, end)
 endfunction
 
--- python3/nvim_uwuifier/uwuifier.py
+++ python3/nvim_uwuifier/uwuifier.py
@@ -6,10 +6,13 @@ class Uwuifier:
     """
     A plugin to uwuify comments in a buffer based on filetype.
     """
-    comment_symbol_lang: dict[str, list[str]] = {
-        '#': ['python', 'sh', 'nasm', 'gas', 'tasm'],
-        '//': ['c', 'cpp', 'go'],
-        '--': ['lua']
+
+    """ test comment """ 
+    # test comment 
+    comment_symbol_lang = {
+        "python": {"comment_char": "#", "multi_line_comment_start": '"""', "multi_line_comment_end": '"""'},
+        "c": {"comment_char": "//", "multi_line_comment_start": "/*", "multi_line_comment_end": "*/"},
+        "cpp": {"comment_char": "//", "multi_line_comment_start": "/*", "multi_line_comment_end": "*/"},
     }
 
     def __init__(self, nvim):
@@ -23,21 +26,56 @@ class Uwuifier:
         buffer = self.nvim.current.buffer
         filetype = self.nvim.eval('&filetype')
 
-        comment_char = ''
-        for symbol, languages in self.comment_symbol_lang.items():
-            if filetype in languages:
-                comment_char = symbol
+        comment_char = ""
+        multi_line_comment_start = ""
+        multi_line_comment_end = ""
+        for language in self.comment_symbol_lang.keys():
+            if filetype == language:
+                language_settings = self.comment_symbol_lang[language]
+                comment_char = language_settings["comment_char"]
+                multi_line_comment_start = language_settings["multi_line_comment_start"]
+                multi_line_comment_end = language_settings["multi_line_comment_end"]
                 break
 
         if not comment_char:
             self.nvim.out_write(f"Filetype: {filetype} is not supported, you can add it manually though.\n")
             return
+        
+        inside_multi_comment = False
 
         for i in range(start_line, end_line):
             line = buffer[i]
             comment_index = line.find(comment_char)
-            if comment_index != -1:
+            if multi_line_comment_start == multi_line_comment_end:
+                start_index = line.find(multi_line_comment_start) if inside_multi_comment == False else -1
+                if start_index != -1:
+                    end_index = line.find(multi_line_comment_end, start_index+len(multi_line_comment_end))
+                else:
+                    end_index = line.find(multi_line_comment_end) if inside_multi_comment == True else -1
+            else:
+                start_index = line.find(multi_line_comment_start)
+                end_index = line.find(multi_line_comment_end)
+           
+            if inside_multi_comment:
+                if end_index != -1:
+                    inside_multi_comment = False
+                    comment = line[:end_index + len(multi_line_comment_end)]
+                    uwuified_comment = self.uwuifier.uwuify(comment)
+                    buffer[i] = uwuified_comment + line[end_index + len(multi_line_comment_end):]
+                else:
+                    comment = line
+                    uwuified_comment = self.uwuifier.uwuify(comment)
+                    buffer[i] = uwuified_comment
+            elif start_index != -1 and end_index != -1:
+                comment = line[start_index + len(multi_line_comment_start):end_index]
+                uwuified_comment = self.uwuifier.uwuify(comment)
+                buffer[i] = line[:start_index + len(multi_line_comment_start)] + uwuified_comment + line[end_index:]
+            elif start_index != -1:
+                inside_multi_comment = True
+                comment = line[start_index + len(multi_line_comment_start):]
+                uwuified_comment = self.uwuifier.uwuify(comment)
+                buffer[i] = line[:start_index + len(multi_line_comment_start)] + uwuified_comment
+            elif comment_index != -1:
                 comment = line[comment_index + len(comment_char):]
                 uwuified_comment = self.uwuifier.uwuify(comment)
                 buffer[i] = line[:comment_index + len(comment_char)] + uwuified_comment
-
