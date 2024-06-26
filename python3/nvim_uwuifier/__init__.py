7bf5f7df34590ddb1f65e07a77df5d6eae7e9eca
Julian Orlowski <julian.sz.orlowski@gmail.com>
2024-06-26 19:34:21 +0200

    Lol, fixed importing

--- python3/nvim_uwuifier/__init__.py
+++ python3/nvim_uwuifier/__init__.py
@@ -5,14 +5,14 @@ print('uwu')
 
 try:
     import pynvim
-except ImportError:
+except Exception:
     print("Pynvim is not installed, installing it now...")
     pip_command = 'pip' if platform.system() == "Windows" else 'pip3'
     subprocess.run([pip_command, 'install', 'pynvim'])
 
 try:
     import uwuipy
-except ImportError:
+except Exception:
     print("Uwuipy is not installed, installing it now...")
     pip_command = 'pip' if platform.system() == "Windows" else 'pip3'
     subprocess.run([pip_command, 'install', 'uwuipy'])
